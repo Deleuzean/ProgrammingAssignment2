15831b78ae69eb45eb7f8e725458bba2a4e41018
Deleuzean <Deleuzean@users.noreply.github.com>
2015-01-23 22:11:17 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Hello! This function can be used to create a matrix in cache, 
+## e.g. a <- makeCacheMatrix, then a$set(matrix(1:10, 5, 5))  which sets a matrix in cache memory
+## You can type a$get() to get that matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+}
+get <- function() x
+setmatrix <- function(solve) m <<- solve
+getmatrix <- function() m
+list(set = set, get = get,
+     setmatrix = setmatrix,
+     getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+##after setting a matrix above, you would type something like cacheSolve(a) or whatever to generate its inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+}
+get <- function() x
+setmatrix <- function(solve) m <<- solve
+getmatrix <- function() m
+list(set = set, get = get,
+     setmatrix = setmatrix,
+     getmatrix = getmatrix)
 }
